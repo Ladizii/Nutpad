9e6ee4e47ebf5412594d99cb0c60cf2bccd7a664
Ladizi <34251071+Ladizii@users.noreply.github.com>
2019-05-05 13:57:49 -0500

    Add files via upload

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour {
+
+    public GameObject insideText;
+    public GameObject thePanel;
+
+    public void ClearText()
+    {
+        insideText.GetComponent<InputField>().text = "";
+    }
+
+    public void CancelButton()
+    {
+        thePanel.SetActive(false);
+    }
+
+    public void ClosedButton()
+    {
+        thePanel.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveLoad : MonoBehaviour
+{
+
+    public string elText;
+    public GameObject ourNote;
+    public GameObject placeHolder;
+    public GameObject saveAnim;
+
+    void Start()
+    {
+        elText = PlayerPrefs.GetString("NoteContents");
+        placeHolder.GetComponent<InputField>().text = elText;
+    }
+
+    public void SaveNote()
+    {
+        elText = ourNote.GetComponent<Text>().text;
+        PlayerPrefs.SetString("NoteContents", elText);
+        StartCoroutine(SaveTextRoll());
+    }
+
+    IEnumerator SaveTextRoll()
+    {
+        saveAnim.GetComponent<Animator>().Play("SavedAnim");
+        yield return new WaitForSeconds(1);
+        saveAnim.GetComponent<Animator>().Play("New State");
+    }
+}
